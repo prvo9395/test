5da8b3fd7c376e35bf0c9e7a5fff38bd049bc7cf
prvo9395 <user3389050@mail.ru>
2025-07-19 13:47:23 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+# Choose nickname
+nickname = input("Choose a nickname: ")
+
+# Connecting to server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('95.164.55.154', 55555))
+
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('utf-8'))
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
